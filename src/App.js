9594f90b8eac6b078a9b5cf4b687b7c856ec8226
CassandraCost <cassandra.costa10@gmail.com>
2023-05-17 21:52:35 -0300

    iniciando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import img from './imagens/user.png';
+import './styles.css'
 
 function App() {
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container"> 
+    
+      <div className="container-login"> 
+
+        <div className="wrap-login"> 
+
+          <form className="login-form">
+
+            <span className="login-form-title">Bem Vindo!</span>
+            <span className="login-form-title">
+            {<img src={img} alt=""/>}
+            </span>
+
+            <div className='wrap-input'>
+              <input className= { (email.length > 0 ? 'has-val ' : '') + 'input'} 
+              type='email'
+              value={email}
+              onChange={e=>setEmail(e.target.value)}/>
+              <span className='focus-input' data-placeholder='Email'></span>
+            </div>
+
+            <div className='wrap-input'>
+              <input className= { (senha.length > 0 ? 'has-val ' : '') + 'input'} 
+              type='password'
+              value={senha}
+              onChange={e=>setSenha(e.target.value)}/>
+              <span className='focus-input' data-placeholder='Senha'></span>
+            </div>
+
+            <div className='container-login-form-btn'>
+              <button className='login-form-btn'>Entrar</button>
+            </div>
+
+            <div className='text-cadastro'>
+              <span className='texto'>Cadastrar-me!</span>
+              <a className='texto2' href='#'>Criar conta</a>
+            </div>
+
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
